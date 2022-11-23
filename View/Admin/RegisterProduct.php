75ca7025161b6fbee8f4f9df5fcb7c9e5149c7de
giovanny12980 <100978182+giovanny12980@users.noreply.github.com>
2022-11-22 19:44:32 -0500

    version1
    version 1

--- View/Admin/RegisterProduct.php
+++ View/Admin/RegisterProduct.php
@@ -27,12 +27,6 @@
                     <a class="nav-link " aria-current="page" href="#">Active</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
                     <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                 </li>
             </ul>
@@ -42,34 +36,74 @@
         <main>
             <!-- Formulario --->
             <div class='shadow'>
-                <form action='' method='post' style='padding: 10px; margin:10px;'>
+                <form action='../../Controller/ProductRegister.php' method='post' style='padding: 10px; margin:10px;'>
                     <h3 style='text-align: center; 'class ='text-success'>Registro de Producto</h3>
                     <div class='form-group'>
+                        <label for=''>Identificacion</label>
+                        <input type='number' class='form-control' id='Idproducto'>
+                    </div>    
+                    <div class='form-group'>
                         <label for='tipo'>Tipo:</label>
-                        <input type='text' class='form-control' id='tipo'>
+                        <input type='text' class='form-control' id='Tipo'>
                     </div>
                     <div class='form-group'>
                         <label for=''>Marca:</label>
-                        <input type='text' class='form-control' id='marca'>
+                        <input type='text' class='form-control' id='Marca'>
+                    </div>
+                    <div class='form-group'>
+                        <label for=''>Genero:</label>
+                        <input type='text' class='form-control' id='Genero'>
                     </div>
                     <div class='form-group'>
                         <label for='coleccion'>Colección o Temporada:</label>
-                        <input type='text' class='form-control' id='coleccion'>
+                        <input type='text' class='form-control' id='Coleccion_Temporada'>
                     </div>
-                    <div class='form-group'>
-                        <label for='coleccion'>Codigo Inventario:</label>
-                        <input type='number' class='form-control' id='coleccion'>
+                    <div>
+                    <span class='input-group-text'>Codigo Inventario:</span>
+                        <select class='form-control' name="idplacainventario" id="idplacainventario">
+                        <option value="1">Seccion tres</option>
+                        <option value="2">Seccion dos</option>
+                        <option value="3">No Disponible</option>
+                        <option value="4">Seccion uno</option>
+                        <option value="5">seccion cuatro</option>
+                        </select>
                     </div>
-                    <div class='form-group'>
-                        <label for='coleccion'>talla:</label>
-                        <input type='numeric' class='form-control' id='coleccion'>
-                    </div>
+                    <div>
+                    <span class='input-group-text'>Talla:</span>
+                        <select class='form-control' name='idtalla' id='idtalla' required>
+                            <option value='1'>Nacional N°38</option>
+                            <option value='2'>Nacional N°39</option>
+                            <option value='3'>Nacional N°40</option>
+                            <option value='4'>Nacional N°41</option>
+                            <option value='5'>USA N°38</option>
+                            <option value='6'>USA N°39</option>
+                            <option value='7'>USA N°40</option>
+                            <option value='8'>USA N°41</option>
+                            <option value='9'>Europea N°38</option>
+                            <option value='10'>Europea N°39</option>
+                            <option value='11'>Europea N°40</option>
+                            <option value='12'>Europea N°41</option>
+                        </select>
+                        </div>
+
                     <div class='input-group mb-3'>
-                        <input class='btn btn-primary' type='submit' value='Registrar Productos' name='registrar' style='float: right;'>
+                        <input class='btn btn-primary' type='submit' value='Registrar Producto' name='registrarProd' style='float: right;'>
                     </div>
                 </form>
             </div>
              <!-- Formulario --->
+             
+            <!-- Tabla de registros--->
+            <div class="row">
+                <div class="col">
+                    <?php 
+                        include "../../Controller/ShowProduct.php";
+                    ?>  
+                </div>  
+                 
+            </div>
+            <!-- Tabla de Registros--->
+            
 
         </main>
     </div>
